e79935c7468aaa69325bce0141c1ab8f05d81268
fra31 <francesco91.croce@gmail.com>
2021-03-01 11:40:41 +0000

    add square for l1, small fix square l2, update utils

--- autoattack/other_utils.py
+++ autoattack/other_utils.py
@@ -1,4 +1,5 @@
 import os
+import torch
 
 class Logger():
     def __init__(self, log_path):
@@ -11,3 +12,37 @@ class Logger():
                 f.write(str_to_log + '\n')
                 f.flush()
             
+def check_imgs(adv, x, norm):
+    delta = (adv - x).view(adv.shape[0], -1)
+    if norm == 'Linf':
+        res = delta.abs().max(dim=1)[0]
+    elif norm == 'L2':
+        res = (delta ** 2).sum(dim=1).sqrt()
+    elif norm == 'L1':
+        res = delta.abs().sum(dim=1)
+
+    str_det = 'max {} pert: {:.5f}, nan in imgs: {}, max in imgs: {:.5f}, min in imgs: {:.5f}'.format(
+        norm, res.max(), (adv != adv).sum(), adv.max(), adv.min())
+    print(str_det)
+    
+    return str_det
+
+def L1_norm(x, keepdim=False):
+    z = x.abs().view(x.shape[0], -1).sum(-1)
+    if keepdim:
+        z = z.view(-1, *[1]*(len(x.shape) - 1))
+    return z
+
+def L2_norm(x, keepdim=False):
+    z = (x ** 2).view(x.shape[0], -1).sum(-1).sqrt()
+    if keepdim:
+        z = z.view(-1, *[1]*(len(x.shape) - 1))
+    return z
+
+def L0_norm(x):
+    return (x != 0.).view(x.shape[0], -1).sum(-1)
+
+def makedir(path):
+    if not os.path.exists(path):
+        os.makedirs(path)
+
--- autoattack/square.py
+++ autoattack/square.py
@@ -15,6 +15,7 @@ import time
 import math
 import torch.nn.functional as F
 
+from autoattack.autopgd_pt import L1_projection
 
 class SquareAttack():
     """
@@ -62,7 +63,8 @@ class SquareAttack():
         self.loss = loss
         self.rescale_schedule = resc_schedule
         self.device = device
-
+        #self.return_all = False
+    
     def margin_and_loss(self, x, y):
         """
         :param y:        correct labels if untargeted else target labels
@@ -84,7 +86,7 @@ class SquareAttack():
             return y_others - y_corr, xent
 
     def init_hyperparam(self, x):
-        assert self.norm in ['Linf', 'L2']
+        assert self.norm in ['Linf', 'L2', 'L1']
         assert not self.eps is None
         assert self.loss in ['ce', 'margin']
 
@@ -125,33 +127,63 @@ class SquareAttack():
             t = (x ** 2).view(x.shape[0], -1).sum(-1).sqrt()
             return x / (t.view(-1, *([1] * self.ndims)) + 1e-12)
 
+        elif self.norm == 'L1':
+            t = x.abs().view(x.shape[0], -1).sum(dim=-1)
+            return x / (t.view(-1, *([1] * self.ndims)) + 1e-12)
+    
     def lp_norm(self, x):
         if self.norm == 'L2':
             t = (x ** 2).view(x.shape[0], -1).sum(-1).sqrt()
             return t.view(-1, *([1] * self.ndims))
 
+        elif self.norm == 'L1':
+            t = x.abs().view(x.shape[0], -1).sum(dim=-1)
+            return t.view(-1, *([1] * self.ndims))
+    
     def eta_rectangles(self, x, y):
         delta = torch.zeros([x, y]).to(self.device)
         x_c, y_c = x // 2 + 1, y // 2 + 1
 
         counter2 = [x_c - 1, y_c - 1]
-        for counter in range(0, max(x_c, y_c)):
-          delta[max(counter2[0], 0):min(counter2[0] + (2*counter + 1), x),
-              max(0, counter2[1]):min(counter2[1] + (2*counter + 1), y)
-              ] += 1.0/(torch.Tensor([counter + 1]).view(1, 1).to(
-              self.device) ** 2)
-          counter2[0] -= 1
-          counter2[1] -= 1
-
-        delta /= (delta ** 2).sum(dim=(0,1), keepdim=True).sqrt()
+        if self.norm == 'L2':
+            for counter in range(0, max(x_c, y_c)):
+              delta[max(counter2[0], 0):min(counter2[0] + (2*counter + 1), x),
+                  max(0, counter2[1]):min(counter2[1] + (2*counter + 1), y)
+                  ] += 1.0/(torch.Tensor([counter + 1]).view(1, 1).to(
+                  self.device) ** 2)
+              counter2[0] -= 1
+              counter2[1] -= 1
     
+            delta /= (delta ** 2).sum(dim=(0, 1), keepdim=True).sqrt()
+        
+        elif self.norm == 'L1':
+            for counter in range(0, max(x_c, y_c)):
+              delta[max(counter2[0], 0):min(counter2[0] + (2*counter + 1), x),
+                  max(0, counter2[1]):min(counter2[1] + (2*counter + 1), y)
+                  ] += 1.0/(torch.Tensor([counter + 1]).view(1, 1).to(
+                  self.device) ** 4)
+              counter2[0] -= 1
+              counter2[1] -= 1
+    
+            delta /= delta.abs().sum(dim=(), keepdim=True)
+        
         return delta
 
     def eta(self, s):
-        delta = torch.zeros([s, s]).to(self.device)
-        delta[:s // 2] = self.eta_rectangles(s // 2, s)
-        delta[s // 2:] = -1. * self.eta_rectangles(s - s // 2, s)
-        delta /= (delta ** 2).sum(dim=(0, 1), keepdim=True).sqrt()
+        if self.norm == 'L2':
+            delta = torch.zeros([s, s]).to(self.device)
+            delta[:s // 2] = self.eta_rectangles(s // 2, s)
+            delta[s // 2:] = -1. * self.eta_rectangles(s - s // 2, s)
+            delta /= (delta ** 2).sum(dim=(0, 1), keepdim=True).sqrt()
+        
+        elif self.norm == 'L1':
+            delta = torch.zeros([s, s]).to(self.device)
+            delta[:s // 2] = self.eta_rectangles(s // 2, s)
+            delta[s // 2:] = -1. * self.eta_rectangles(s - s // 2, s)
+            #delta = self.eta_rectangles(s, s)
+            delta /= delta.abs().sum(dim=(), keepdim=True)
+            #delta *= (torch.rand([1]) - .5).sign().to(self.device)
+        
         if torch.rand([1]) > 0.5:
             delta = delta.permute([1, 0])
 
@@ -312,7 +344,8 @@ class SquareAttack():
 
                     norms_image = self.lp_norm(x_best_curr - x_curr)
                     mask_image = torch.max(new_deltas_mask, new_deltas_mask_2)
-                    norms_windows = self.lp_norm(delta_curr * mask_image)
+                    norms_windows = ((delta_curr * mask_image) ** 2).sum(dim=(
+                        -2, -1), keepdim=True).sqrt()
 
                     new_deltas = torch.ones([x_curr.shape[0], c, s, s]
                         ).to(self.device)
@@ -373,6 +406,127 @@ class SquareAttack():
                     if ind_succ.numel() == n_ex_total:
                         break
 
+            elif self.norm == 'L1':
+                delta_init = torch.zeros_like(x)
+                s = h // 5
+                sp_init = (h - s * 5) // 2
+                vh = sp_init + 0
+                for _ in range(h // s):
+                    vw = sp_init + 0
+                    for _ in range(w // s):
+                        delta_init[:, :, vh:vh + s, vw:vw + s] += self.eta(
+                            s).view(1, 1, s, s) * self.random_choice(
+                            [x.shape[0], c, 1, 1])
+                        vw += s
+                    vh += s
+
+                #x_best = torch.clamp(x + self.normalize(delta_init
+                #    ) * self.eps, 0., 1.)
+                r_best = L1_projection(x, delta_init, self.eps * (1. - 1e-6))
+                x_best = x + delta_init + r_best
+                margin_min, loss_min = self.margin_and_loss(x_best, y)
+                n_queries = torch.ones(x.shape[0]).to(self.device)
+                s_init = int(math.sqrt(self.p_init * n_features / c))
+
+                for i_iter in range(self.n_queries):
+                    idx_to_fool = (margin_min > 0.0).nonzero().squeeze()
+
+                    x_curr = self.check_shape(x[idx_to_fool])
+                    x_best_curr = self.check_shape(x_best[idx_to_fool])
+                    y_curr = y[idx_to_fool]
+                    if len(y_curr.shape) == 0:
+                        y_curr = y_curr.unsqueeze(0)
+                    margin_min_curr = margin_min[idx_to_fool]
+                    loss_min_curr = loss_min[idx_to_fool]
+
+                    delta_curr = x_best_curr - x_curr
+                    p = self.p_selection(i_iter)
+                    s = max(int(round(math.sqrt(p * n_features / c))), 3)
+                    if s % 2 == 0:
+                        s += 1
+                        #pass
+                    
+                    vh = self.random_int(0, h - s)
+                    vw = self.random_int(0, w - s)
+                    new_deltas_mask = torch.zeros_like(x_curr)
+                    new_deltas_mask[:, :, vh:vh + s, vw:vw + s] = 1.0
+                    norms_window_1 = delta_curr[:, :, vh:vh + s, vw:vw + s
+                        ].abs().sum(dim=(-2, -1), keepdim=True)
+
+                    vh2 = self.random_int(0, h - s)
+                    vw2 = self.random_int(0, w - s)
+                    new_deltas_mask_2 = torch.zeros_like(x_curr)
+                    new_deltas_mask_2[:, :, vh2:vh2 + s, vw2:vw2 + s] = 1.
+
+                    norms_image = self.lp_norm(x_best_curr - x_curr)
+                    mask_image = torch.max(new_deltas_mask, new_deltas_mask_2)
+                    norms_windows = (delta_curr * mask_image).abs().sum(dim=(
+                        -2, -1), keepdim=True)
+
+                    new_deltas = torch.ones([x_curr.shape[0], c, s, s]
+                        ).to(self.device)
+                    new_deltas *= (self.eta(s).view(1, 1, s, s) *
+                        self.random_choice([x_curr.shape[0], c, 1, 1]))
+                    old_deltas = delta_curr[:, :, vh:vh + s, vw:vw + s] / (
+                        1e-12 + norms_window_1)
+                    new_deltas += old_deltas
+                    new_deltas = new_deltas / (1e-12 + new_deltas.abs().sum(
+                        dim=(-2, -1), keepdim=True)) * (torch.max(
+                        self.eps * torch.ones_like(norms_image) -
+                        norms_image, torch.zeros_like(norms_image)) /
+                        c + norms_windows) * c
+                    delta_curr[:, :, vh2:vh2 + s, vw2:vw2 + s] = 0.
+                    delta_curr[:, :, vh:vh + s, vw:vw + s] = new_deltas + 0
+
+                    #
+                    #norms_image_old = self.lp_norm(delta_curr)
+                    r_curr = L1_projection(x_curr, delta_curr, self.eps * (1. - 1e-6))
+                    x_new = x_curr + delta_curr + r_curr
+                    x_new = self.check_shape(x_new)
+                    norms_image = self.lp_norm(x_new - x_curr)
+
+                    margin, loss = self.margin_and_loss(x_new, y_curr)
+
+                    # update loss if new loss is better
+                    idx_improved = (loss < loss_min_curr).float()
+
+                    loss_min[idx_to_fool] = idx_improved * loss + (
+                        1. - idx_improved) * loss_min_curr
+
+                    # update margin and x_best if new loss is better
+                    # or misclassification
+                    idx_miscl = (margin <= 0.).float()
+                    idx_improved = torch.max(idx_improved, idx_miscl)
+
+                    margin_min[idx_to_fool] = idx_improved * margin + (
+                        1. - idx_improved) * margin_min_curr
+                    idx_improved = idx_improved.reshape([-1,
+                        *[1]*len(x.shape[:-1])])
+                    x_best[idx_to_fool] = idx_improved * x_new + (
+                        1. - idx_improved) * x_best_curr
+                    n_queries[idx_to_fool] += 1.
+
+                    ind_succ = (margin_min <= 0.).nonzero().squeeze()
+                    if self.verbose and ind_succ.numel() != 0:
+                        print('{}'.format(i_iter + 1),
+                            '- success rate={}/{} ({:.2%})'.format(
+                            ind_succ.numel(), n_ex_total, float(
+                            ind_succ.numel()) / n_ex_total),
+                            '- avg # queries={:.1f}'.format(
+                            n_queries[ind_succ].mean().item()),
+                            '- med # queries={:.1f}'.format(
+                            n_queries[ind_succ].median().item()),
+                            '- loss={:.3f}'.format(loss_min.mean()),
+                            '- max pert={:.3f}'.format(norms_image.max().item()),
+                            #'- old pert={:.3f}'.format(norms_image_old.max().item())
+                            )
+                    
+                    assert (x_new != x_new).sum() == 0
+                    assert (x_best != x_best).sum() == 0
+        
+                    if ind_succ.numel() == n_ex_total:
+                        break
+        
         return n_queries, x_best
 
     def perturb(self, x, y=None):
@@ -387,6 +541,7 @@ class SquareAttack():
         self.init_hyperparam(x)
 
         adv = x.clone()
+        #adv_all = x.clone()
         if y is None:
             if not self.targeted:
                 with torch.no_grad():
@@ -431,11 +586,16 @@ class SquareAttack():
 
                 acc[ind_to_fool[ind_curr]] = 0
                 adv[ind_to_fool[ind_curr]] = adv_curr[ind_curr].clone()
+                #adv_all[ind_to_fool] = adv_curr.clone()
                 if self.verbose:
                     print('restart {} - robust accuracy: {:.2%}'.format(
                         counter, acc.float().mean()),
                         '- cum. time: {:.1f} s'.format(
                         time.time() - startt))
 
-        return adv
+        if not self.return_all:
+            return adv
+        else:
+            print('returning final points')
+            return adv_all
 
